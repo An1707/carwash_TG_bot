84e3f608feea6a56c736cd1c8f39bf7afedc25db
An1707 <61923166+An1707@users.noreply.github.com>
2024-10-02 22:54:46 +0300

    Update car_wash_bot.py

--- car_wash_bot.py
+++ car_wash_bot.py
@@ -1,1 +1,20 @@
+import telebot
+from config import TOKEN
+from user import handle_user_commands
+from admin import handle_admin_commands
+from database import init_db
+
+bot = telebot.TeleBot(TOKEN)
+
+# Инициализация базы данных при запуске
+init_db()
+
+# Обработка команд пользователя
+handle_user_commands(bot)
+
+# Обработка команд администратора
+handle_admin_commands(bot)
+
+# Запуск бота
+bot.polling()
 
