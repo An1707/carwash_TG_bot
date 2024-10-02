5d7fff48d6a972c25c1db655ed9d4c1d9ab3e8ad
An1707 <61923166+An1707@users.noreply.github.com>
2024-10-02 22:56:30 +0300

    Update user.py

--- user.py
+++ user.py
@@ -1,1 +1,13 @@
+from telebot import TeleBot
+from database import get_services, register_user, book_appointment
+
+def handle_user_commands(bot: TeleBot):
+    @bot.message_handler(commands=['start'])
+    def start(message):
+        user_id = message.from_user.id
+        # Логика регистрации и обработки пользователей
+
+    @bot.message_handler(commands=['book'])
+    def book_service(message):
+        # Логика записи пользователя на услугу
 
